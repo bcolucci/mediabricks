0931a6d39c014ba47ea00fb0d7ee268339dfb7ac
Brice Colucci <brice.colucci@gmail.com>
2023-04-21 11:30:45 -0400

    wip

--- App.js
+++ App.js
@@ -1,42 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { StyleSheet, Text, View, Share } from "react-native";
-import { Provider as PaperProvider, Button } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
 import * as Linking from "expo-linking";
 
 export default function App() {
-  const linkUrl = Linking.useURL();
-  const url =
-    "https://play.google.com/store/apps/details?id=com.instagram.android&hl=en_IN&gl=US";
+  const url = Linking.useURL();
   useEffect(() => {
-    console.log("link url", linkUrl);
-  });
-  const onShare = async () => {
-    try {
-      const result = await Share.share({
-        message: "Instagram | A time wasting application" + "\n" + url,
-      });
-      if (result.action === Share.sharedAction) {
-        if (result.activityType) {
-          // shared with activity type of result.activityType
-        } else {
-          // shared
-        }
-      } else if (result.action === Share.dismissedAction) {
-        // dismissed
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+    console.log("url", url);
+  }, [url]);
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>It works!</Text>
-        <Button icon="camera" mode="contained" onPress={onShare}>
-          Press me
-        </Button>
-        <StatusBar style="auto" />
+        <Text>It works! {new Date().toLocaleDateString()}</Text>
       </View>
     </PaperProvider>
   );
